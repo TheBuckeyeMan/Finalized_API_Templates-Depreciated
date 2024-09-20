6eab872ea92ef4e2daaddc658b4566aa996a621a
Adam Maas <adamjm1220@gmail.com>
2024-09-20 10:57:48 -0400

    Lambda Function Template

--- src/main/java/base/template/template/TemplateApplication.java
+++ src/main/java/base/template/template/TemplateApplication.java
@@ -1,12 +1,24 @@
 package base.template.template;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import base.template.template.service.TemplateService;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
-public class TemplateApplication {
+@RequiredArgsConstructor
+public class TemplateApplication implements CommandLineRunner{
+
+	private final TemplateService templateService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		templateService.get(); //Call API on Startup
+		System.exit(0); //Exit API After API call is made
 	}
 }
--- src/main/java/base/template/template/api/handler/LambdaHandler.java
+++ src/main/java/base/template/template/api/handler/LambdaHandler.java
@@ -1,8 +1,12 @@
 package base.template.template.api.handler;
 
+import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
 
-//Lambda Handler to handle Lambda Config
+//Lambda Handler using FunctionInvoker will invoke the nessasary functions in the application.
+//With Function invoker, we created a function in TemplateService and will invoke this per lambda
 
-public class LambdaHandler {
+
+public class LambdaHandler extends FunctionInvoker{
+    
     
 }
