7d06feebd23ac05717a158da453e5ee087627d33
Adam Maas <adamjm1220@gmail.com>
2024-09-27 09:33:20 -0400

    Lambda Update

--- src/main/java/base/template/template/api/handler/LambdaHandler.java
+++ src/main/java/base/template/template/api/handler/LambdaHandler.java
@@ -2,6 +2,6 @@ package base.template.template.api.handler;
 
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
 
-//Lambda Handler using FunctionInvoker will invoke the nessasary functions in the application.
-//With Function invoker, we created a function in TemplateService and will invoke this per lambda
+//Lambda Handler is essentially the main class for lambda functions. The handleRequest method within the class is the entrypoint to the lambda. 
+
 public class LambdaHandler extends FunctionInvoker{}
